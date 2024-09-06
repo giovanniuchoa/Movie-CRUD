60a95d6b112c602268aa3e86d3ce6f52f02d1231
Giovanni Lucca Reque Uchoa <giovanni.uchoa@outlook.com>
2024-09-05 22:16:50 -0300

    organizing code
    giovanni

--- obj/Debug/net8.0/Movie-CRUD.AssemblyInfo.cs
+++ obj/Debug/net8.0/Movie-CRUD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movie-CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6c48e1af0f0b5d953257983d17d6b164ab82673")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f85b95d7ae75640a59bced18c9a559e3b5821388")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movie-CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movie-CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
